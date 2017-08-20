60139afaabdc15cc85bd4381f61f7c6fad585852
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2017-08-21 00:14:53 +0300

    Update validation with messages, add tests

--- src/validation.js
+++ src/validation.js
@@ -1,21 +1,53 @@
+import { formatRules, getRule } from './rules'
+import formatErrorMessage from './messages'
+
+// Validate value against rules, return errors array
+function validateValue(value, rules) {
+  return rules.reduce((existingErrors, rule) => {
+    const isValid = getRule(rule.title)(value)
+
+    return !isValid
+      ? [...existingErrors, formatErrorMessage(rule.title, rule.params)]
+      : existingErrors
+  }, [])
+}
+
+// Validate fields against rules, return field with array of errors if found
+function validateFields(fields, rules) {
+  return Object.keys(fields).reduce((fieldsWithErrors, currentField) => {
+    const fieldValue = fields[currentField]
+    const fieldErrors = validateValue(fieldValue, rules)
+
+    return fieldErrors.length
+      ? { ...fieldsWithErrors, [currentField]: fieldErrors }
+      : fieldsWithErrors
+  }, {})
+}
+
 // Proceed the first part of field rules, return object with fields and their values or errors
 function proceedRuleFirstPart(partTitle, partRules, data, isLastPart) {
   if (partTitle === '*') {
     if (Array.isArray(data) && data.length) {
-      return data.reduce((fields, value, valueIndex) => ({
-        ...fields,
-        [valueIndex]: value // validate value
-      }), {})
+      return data.reduce((fields, value, valueIndex) => {
+        const field = ({ [valueIndex]: value })
+
+        return isLastPart
+          ? { ...fields, ...validateFields(field, partRules) }
+          : { ...fields, ...field }
+      }, {})
     }
 
     return {}
   }
 
-  return { [partTitle]: data ? data[partTitle] : undefined } // validate value
+  const fieldValue = data ? data[partTitle] : undefined
+  const field = ({ [partTitle]: fieldValue })
+
+  return isLastPart ? validateFields(field, partRules) : field
 }
 
 // Proceed the rest part of field rules, return object with fields and their values or errors
-function parseRuleRestPart(part, fields, isLastPart) {
+function proceedRuleRestPart(part, fields, rules, isLastPart) {
   const fieldsArray = Object.keys(fields)
 
   if (fieldsArray.length) {
@@ -26,20 +58,25 @@ function parseRuleRestPart(part, fields, isLastPart) {
         if (Array.isArray(currentFieldValue) && currentFieldValue.length) {
           return {
             ...parsedFields,
-            ...currentFieldValue.reduce((parsedValues, currentValue, currentValueIndex) => ({
-              ...parsedValues,
-              [`${currentField}.${currentValueIndex}`]: currentValue // validate value
-            }), {})
+            ...currentFieldValue.reduce((parsedValues, currentValue, currentValueIndex) => {
+              const field = ({ [`${currentField}.${currentValueIndex}`]: currentValue })
+
+              return isLastPart
+                ? { ...parsedValues, ...validateFields(field, rules) }
+                : { ...parsedValues, ...field }
+            }, {})
           }
         }
 
         return parsedFields
       }
 
-      return {
-        ...parsedFields,
-        [`${currentField}.${part}`]: currentFieldValue ? currentFieldValue[part] : undefined // validate value
-      }
+      const fieldValue = currentFieldValue ? currentFieldValue[part] : undefined
+      const field = ({ [`${currentField}.${part}`]: fieldValue })
+
+      return isLastPart
+        ? { ...parsedFields, ...validateFields(field, rules) }
+        : { ...parsedFields, ...field }
     }, {})
   }
 
@@ -55,16 +92,18 @@ function validateField(field, rules, data) {
       return proceedRuleFirstPart(currentRulePart, rules, data, isLastPart)
     }
 
-    return parseRuleRestPart(currentRulePart, fields, isLastPart)
+    return proceedRuleRestPart(currentRulePart, fields, rules, isLastPart)
   }, data)
 }
 
 // Validate data with rules, return object with fields and their errors
 export default function validate(data, rules) {
-  const fieldsWithValues = Object.keys(rules).reduce((fieldsWithErrors, currentField) => ({
+  const parsedRules = formatRules(rules)
+
+  const fieldsWithValues = Object.keys(parsedRules).reduce((fieldsWithErrors, currentField) => ({
     ...fieldsWithErrors,
-    ...validateField(currentField, rules[currentField], data)
+    ...validateField(currentField, parsedRules[currentField], data)
   }), {})
 
-  console.log(fieldsWithValues)
+  return fieldsWithValues
 }
---
+++ tests/validation.test.js
@@ -0,0 +1,76 @@
+import validate from '../src/validation'
+
+describe('Validation', () => {
+  test('should return errors when object field is incorrect', () => {
+    const data = {
+      name: ''
+    }
+    const rules = {
+      name: 'required'
+    }
+    const expectedResult = {
+      name: ['This field is required']
+    }
+
+    expect(validate(data, rules)).toMatchObject(expectedResult)
+  })
+
+  test('should return errors when object fields are incorrect', () => {
+    const data = {
+      name: '',
+      wife: {
+        name: 'Susan'
+      }
+    }
+    const rules = {
+      name: 'required',
+      'wife.name': 'required',
+      'wife.pets': 'required'
+    }
+    const expectedResult = {
+      name: ['This field is required'],
+      'wife.pets': ['This field is required']
+    }
+
+    expect(validate(data, rules)).toMatchObject(expectedResult)
+  })
+
+  test('should resolve wildcards and return errors when object fields are incorrect', () => {
+    const data = {
+      pets: ['Rex', null, [[[], {}, { name: 'Tiger' }]]]
+    }
+    const rules = {
+      'pets.*': 'array',
+      'pets.*.*.*.name': 'required',
+      'cats.*': 'required'
+    }
+    const expectedResult = {
+      'pets.0': ['This field must be an array'],
+      'pets.1': ['This field must be an array'],
+      'pets.2.0.0.name': ['This field is required'],
+      'pets.2.0.1.name': ['This field is required']
+    }
+
+    expect(validate(data, rules)).toMatchObject(expectedResult)
+  })
+
+  test('should return errors when array is incorrect', () => {
+    const data = ['', [1], { name: 'Rex' }]
+    const rules = {
+      '*': 'array',
+      '*.*': 'array',
+      '*.name': 'required',
+      '*.*.name': 'required'
+    }
+    const expectedResult = {
+      0: ['This field must be an array'],
+      2: ['This field must be an array'],
+      '1.0': ['This field must be an array'],
+      '0.name': ['This field is required'],
+      '1.name': ['This field is required'],
+      '1.0.name': ['This field is required']
+    }
+
+    expect(validate(data, rules)).toMatchObject(expectedResult)
+  })
+})
