4a12c37a83d99768902977c0fc88051fc855f577
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2018-02-06 15:13:22 +0300

    Update alpha rule and tests

--- tests/rules/alpha.test.js
+++ tests/rules/alpha.test.js
@@ -1,28 +1,20 @@
 import alpha from '../../src/rules/alpha'
 
 describe('alpha rule', () => {
-  test('should be true when value is a non-empty string with a-z, A-Z characters', () => {
+  test('should be true when value is a string with only alphabetic characters', () => {
     expect(alpha('Jack')).toBe(true)
   })
 
-  test('should be false when value is an empty string', () => {
-    expect(alpha('')).toBe(false)
+  test('should be false when value is a string with non-alphabetic characters', () => {
+    expect(alpha('area51')).toBe(false)
   })
 
-  test('should be false when value is a string with white space', () => {
+  test('should be true when value is an empty string', () => {
+    expect(alpha('')).toBe(true)
+  })
+
+  test('should be false when value is a string with only white space', () => {
     expect(alpha(' ')).toBe(false)
-  })
-
-  test('should be false when value is a string with a number', () => {
-    expect(alpha('9')).toBe(false)
-  })
-
-  test('should be false when value is a string with a dash', () => {
-    expect(alpha('-')).toBe(false)
-  })
-
-  test('should be false when value is a string with an underscore', () => {
-    expect(alpha('_')).toBe(false)
   })
 
   test('should be false when value is a number', () => {
@@ -39,6 +31,10 @@ describe('alpha rule', () => {
 
   test('should be false when value is an object', () => {
     expect(alpha({})).toBe(false)
+  })
+
+  test('should be false when value is undefined', () => {
+    expect(alpha(undefined)).toBe(false)
   })
 
   test('should be false when value is null', () => {
