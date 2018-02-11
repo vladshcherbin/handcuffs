da7e25729604d7eea8c7e379ec14cfff02eb2723
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2018-02-11 08:59:23 +0300

    Update rule functions

--- src/rules.js
+++ src/rules.js
@@ -1,7 +1,8 @@
+import { addMessage } from './messages'
 import accepted from './rules/accepted'
 import alpha from './rules/alpha'
-import alphaDash from './rules/alphaDash'
 import alphaNum from './rules/alphaNum'
+import alphaNumDash from './rules/alphaNumDash'
 import array from './rules/array'
 import between from './rules/between'
 import boolean from './rules/boolean'
@@ -11,12 +12,11 @@ import numeric from './rules/numeric'
 import required from './rules/required'
 import string from './rules/string'
 
-// Predefined rules
 const validationRules = {
   accepted,
   alpha,
-  alphaDash,
   alphaNum,
+  alphaNumDash,
   array,
   between,
   boolean,
@@ -29,84 +29,91 @@ const validationRules = {
 
 const numericRules = ['numeric']
 
-// Add a new rule
-export function addRule(ruleTitle, ruleFunction) {
-  if (validationRules[ruleTitle]) {
-    throw new Error(`Validation rule "${ruleTitle}" already exists`)
-  } else {
-    validationRules[ruleTitle] = ruleFunction
+export function addRule(title, ruleFunction, message) {
+  if (!title || !ruleFunction || !message) {
+    throw new Error('No title, function or message was provided for custom rule')
   }
+
+  if (validationRules[title]) {
+    throw new Error(`Validation rule '${title}' already exists`)
+  }
+
+  validationRules[title] = ruleFunction
+
+  addMessage(title, message)
 }
 
-// Get defined rule
-export function getRule(ruleTitle) {
-  if (!validationRules[ruleTitle]) {
-    throw new Error(`Validation rule "${ruleTitle}" doesn't exist`)
-  } else {
-    return validationRules[ruleTitle]
-  }
+export function parseRules(rules) {
+  return rules.split('|').map((rule) => {
+    const [title, params] = rule.split(':')
+
+    return {
+      title,
+      params: params ? params.split(',') : []
+    }
+  })
 }
 
-// Check if rules array has any of provided rules
-export function hasRules(rulesArray, rulesToFind) {
-  const ruleTitlesArray = rulesArray.map(rule => rule.title)
+export function getRuleValidationFunction(rule) {
+  const validationFunction = validationRules[rule]
+
+  if (!validationFunction) {
+    throw new Error(`No validation function was defined for '${rule}' rule`)
+  }
+
+  return validationFunction
+}
+
+function rulesContain(rules, rulesToFind) {
+  const ruleTitlesArray = rules.map(rule => rule.title)
 
   if (Array.isArray(rulesToFind)) {
-    return rulesToFind.reduce((result, currentRuleToFind) => (
-      result || ruleTitlesArray.includes(currentRuleToFind)
-    ), false)
+    let ruleWasFound = false
+
+    for (let i = 0; i < rulesToFind.length; i += 1) {
+      if (ruleTitlesArray.includes(rulesToFind[i])) {
+        ruleWasFound = true
+
+        break
+      }
+    }
+
+    return ruleWasFound
   }
 
   return ruleTitlesArray.includes(rulesToFind)
 }
 
-// Check if rules array has any of numeric rules
-export function hasNumericRules(rulesArray) {
-  return hasRules(rulesArray, numericRules)
+function rulesContainNumericRules(rules) {
+  return rulesContain(rules, numericRules)
 }
 
-// Check required params for a rule
-export function requireParamsCount(count, params, rule) {
-  if (!Array.isArray(params) || params.length < count) {
-    throw new Error(`Validation rule "${rule}" requires at least ${count} parameters`)
+export function getRuleValueType(rules) {
+  if (rulesContainNumericRules(rules)) {
+    return 'numeric'
+  }
+
+  if (rulesContain(rules, 'array')) {
+    return 'array'
+  }
+
+  return 'string'
+}
+
+export function checkRuleParamsCount(params, requiredParamsCount, rule) {
+  if (!Array.isArray(params) || params.length < requiredParamsCount) {
+    throw new Error(`'${rule}' rule requires at least ${requiredParamsCount} parameters`)
   }
 }
 
-// Split rule title and params, located after ':', divided by ','
-function extractRuleParams(rule) {
-  const [ruleTitle, ruleParams] = rule.split(':')
-  const params = ruleParams ? ruleParams.split(',') : []
-
-  return {
-    title: ruleTitle,
-    params
-  }
-}
-
-// Get array of rules from string, divided by '|'
-function formatFieldRules(rules) {
-  return rules.split('|').map(rule => extractRuleParams(rule))
-}
-
-// Parse provided array of string rules
-export function formatRules(rules) {
-  return Object.keys(rules).reduce((existingRules, field) => ({
-    ...existingRules,
-    [field]: formatFieldRules(rules[field])
-  }), {})
-}
-
-// Get the size of the value, depending on the value type and rules
-export function getSize(value, rules) {
-  const hasNumericRule = hasNumericRules(rules)
-
-  if (numeric(value) && hasNumericRule) {
-    return value
+export function getValueSize(value, rules) {
+  if (numeric(value) && rules && rulesContainNumericRules(rules)) {
+    return Number(value)
   }
 
   if (Array.isArray(value)) {
     return value.length
   }
 
-  return String(value).length
+  return String(value).trim().length
 }
