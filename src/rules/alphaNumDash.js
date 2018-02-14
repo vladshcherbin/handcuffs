004c71a963aa454914eb2f0fbb54f7aad584c80b
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2018-02-14 19:38:05 +0300

    Fix rule titles

--- src/rules/alphaNumDash.js
+++ src/rules/alphaNumDash.js
@@ -1,6 +1,6 @@
 import isNumber from 'is-number'
 import string from './string'
 
-export default function alphaNum(value) {
+export default function alphaNumDash(value) {
   return (string(value) || isNumber(value)) && (/^$|^[a-z0-9_-]+$/i).test(value)
 }
--- src/rules/max.js
+++ src/rules/max.js
@@ -1,6 +1,6 @@
 import { checkRuleParamsCount, getValueSize } from '../rules'
 
-export default function min(value, params, rules) {
+export default function max(value, params, rules) {
   checkRuleParamsCount(params, 1, 'max')
 
   const valueSize = getValueSize(value, rules)
