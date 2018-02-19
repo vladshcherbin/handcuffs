7603a7a893759d73aef356777b028a5c9918a4f0
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2018-02-19 12:02:47 +0300

    Remove separate config for browser and node

--- rollup.config.js
+++ rollup.config.js
@@ -3,41 +3,35 @@ import cleanup from 'rollup-plugin-cleanup'
 import prettier from 'rollup-plugin-prettier'
 import filesize from 'rollup-plugin-filesize'
 
-const config = (target) => {
-  const folder = target
-
-  return {
-    input: 'src/index.js',
-    output: [
-      {
-        file: `dist/${folder}/cjs/index.js`,
-        format: 'cjs'
-      },
-      {
-        file: `dist/${folder}/es/index.js`,
-        format: 'es'
-      }
-    ],
-    plugins: [
-      babel({
-        exclude: 'node_modules/**',
-        babelrc: false
-      }),
-      cleanup({
-        comments: 'none',
-        maxEmptyLines: 1
-      }),
-      prettier({
-        printWidth: 100,
-        singleQuote: true
-      }),
-      filesize()
-    ],
-    external: ['dot-prop-wild', 'is-number']
-  }
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: 'dist/cjs/index.js',
+      format: 'cjs'
+    },
+    {
+      file: 'dist/es/index.js',
+      format: 'es'
+    }
+  ],
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+      babelrc: false
+    }),
+    cleanup({
+      comments: 'none',
+      maxEmptyLines: 1
+    }),
+    prettier({
+      printWidth: 100,
+      singleQuote: true
+    }),
+    filesize()
+  ],
+  external: [
+    'dot-prop-wild',
+    'is-number'
+  ]
 }
-
-export default [
-  config('node'),
-  config('browser')
-]
